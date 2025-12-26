21b07218ca05c77db2fba30b615f40c32863aabb
CrisisUp <cristiano.1973.on@gmail.com>
2025-12-26 12:10:39 -0300

    third commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,8 @@
-import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
-import { Container } from './components/Container';
-import { CountDown } from './components/CountDown';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { DefaultInput } from './components/DefaultInput';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
+import { Home } from './pages/Home';
 
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              labelText=''
-              id=''
-              type=''
-              placeholder='Digite algo...'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} color='green' />
-            <DefaultButton icon={<StopCircleIcon />} color='red' />
-          </div>
-        </form>
-      </Container>
-    </>
-  );
+  return <Home />;
 }
